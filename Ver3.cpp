1fba5e3111fda25220846b61ff0586c99f761b91
Zinawe <121714420+KostKost777@users.noreply.github.com>
2025-09-18 22:10:26 +0300

    Add files via upload

--- Ver3.cpp
+++ Ver3.cpp
@@ -7,12 +7,12 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-const int BEGINOFLINE = 0;
-const int ENDOFLINE = 1;
+typedef int (*Compare_Func_t)(struct Struct_Line str1,
+                              struct Struct_Line str2);
 
 struct Struct_Line{
     char* line_ptr;
-    int str_len;
+    size_t str_len;
 };
 
 struct Struct_Poem{
@@ -36,27 +36,28 @@ ssize_t GetSizeOfFile(const char* filename);
 
 size_t CountSymbol(struct Struct_Poem* Poem, char sym);
 
-int SetPoemStructFromFile(struct Struct_Poem* Poem,
+int ReadPoemStructFromFile(struct Struct_Poem* Poem,
                           const char* input_filename);
 
 void FreeDataPoem(struct Struct_Poem* Poem);
 
 void BubbleSort(struct Struct_Poem* Poem,
-                int (*comparator)(struct Struct_Line str1,
-                                  struct Struct_Line str2));
+                Compare_Func_t Comparator);
 
-int MyStrcmpByFirstChars(struct Struct_Line str1,
+int CompareByFirstChars(struct Struct_Line str1,
                          struct Struct_Line str2);
 
 void SwapStrings(struct Struct_Line* str1,
                  struct Struct_Line* str2);
 
-int MyStrcmpByLastChars(struct Struct_Line str1,
-                         struct Struct_Line str2);
+int CompareByLastChars(struct Struct_Line str1,
+                        struct Struct_Line str2);
 
-size_t LineEndIndex(char* str);
+size_t GetLineEndIndex(char* str);
 
-size_t GetIndexFirstAlpha(char* str, const int start_point);
+size_t GoForward(struct Struct_Line str);
+
+size_t GoBackwards(struct Struct_Line str);
 
 int main(){
     const char* input_filename = "poem_orig.txt";
@@ -65,14 +66,14 @@ int main(){
 
     struct Struct_Poem Poem_Onegin = {};
 
-    if (SetPoemStructFromFile(&Poem_Onegin, input_filename)  == -1)
+    if (ReadPoemStructFromFile(&Poem_Onegin, input_filename)  == -1)
         return 1;
 
     fprintf(out_file, "\n\n\n****************"
                       "Sorting by beginning of line"
                       "*******************\n\n\n");
 
-    BubbleSort(&Poem_Onegin, &MyStrcmpByFirstChars);
+    BubbleSort(&Poem_Onegin, &CompareByFirstChars);
 
     if (PrintInFilePoem(&Poem_Onegin, out_file, out_file_name)  == -1)
         return 1;
@@ -81,7 +82,7 @@ int main(){
                       "Sorting by end of line"
                       "*******************\n\n\n");
 
-    BubbleSort(&Poem_Onegin, &MyStrcmpByLastChars);
+    BubbleSort(&Poem_Onegin, &CompareByLastChars);
 
     if (PrintInFilePoem(&Poem_Onegin, out_file, out_file_name)  == -1)
         return 1;
@@ -90,15 +91,18 @@ int main(){
                       "Original Poem"
                       "*******************\n\n\n");
 
-    fprintf(out_file, Poem_Onegin.buffer);
+    fprintf(out_file, "%s", Poem_Onegin.buffer);
 
     FreeDataPoem(&Poem_Onegin);
     fclose(out_file);
 }
 
-int SetPoemStructFromFile(struct Struct_Poem* Poem,
+int ReadPoemStructFromFile(struct Struct_Poem* Poem,
                           const char* input_filename)
 {
+    assert(Poem != NULL);
+    assert(input_filename != NULL);
+
     int file_descriptor = open(input_filename, O_RDONLY);
 
     if (file_descriptor == -1) {
@@ -118,7 +122,7 @@ int SetPoemStructFromFile(struct Struct_Poem* Poem,
     //printf("%d\n", Poem->size_of_file);
     Poem->size_of_file = read(file_descriptor, Poem->buffer, Poem->size_of_file);
 
-    if(Poem->size_of_file == -1){
+    if (Poem->size_of_file == -1) {
         fprintf(stderr, "Error reading file |%s|", input_filename);
         close(file_descriptor);
         perror("");
@@ -128,7 +132,8 @@ int SetPoemStructFromFile(struct Struct_Poem* Poem,
 
     Poem->number_of_lines = CountSymbol(Poem, '\n');
 
-    Poem->poem_ptr_array = (struct Struct_Line*)calloc(Poem->number_of_lines, sizeof(struct Struct_Line));
+    Poem->poem_ptr_array = (struct Struct_Line*)calloc(Poem->number_of_lines,
+                                                       sizeof(struct Struct_Line));
     assert(Poem->poem_ptr_array != NULL);
 
     //ReplaceSymbolInBuffer(Poem, '\n', '\0');
@@ -149,7 +154,7 @@ size_t CountSymbol(struct Struct_Poem* Poem, char sym)
     assert(Poem->buffer != NULL);
 
     int counter_enter = 0;
-    for (int i = 0; i < Poem->size_of_file; ++i){
+    for (int i = 0; i < Poem->size_of_file; ++i) {
        // printf("[%d] - %c\n", (Poem->buffer)[i], (Poem->buffer)[i]);
         if ((Poem->buffer)[i] == sym)
             counter_enter++;
@@ -167,9 +172,9 @@ void CopyFromBufferInRaggedArray(struct Struct_Poem* Poem)
     char* now_ptr = NULL;
     size_t buffer_index = 0;
     size_t poem_index = 0;
-    for (; poem_index < Poem->number_of_lines; ++buffer_index){
+    for (; poem_index < Poem->number_of_lines; ++buffer_index) {
         //printf("%d\n", (Poem->buffer)[buffer_index]);
-        if ((Poem->buffer)[buffer_index] == '\n'){
+        if ((Poem->buffer)[buffer_index] == '\n') {
             now_ptr = &(Poem->buffer)[buffer_index];
             (Poem->poem_ptr_array[poem_index].line_ptr) = line_begin_ptr;
 
@@ -191,7 +196,7 @@ void ReplaceSymbolInBuffer(struct Struct_Poem* Poem,
     assert(Poem != NULL);
     assert(Poem->buffer != NULL);
 
-    for (int i = 0; i < Poem->size_of_file; ++i){
+    for (int i = 0; i < Poem->size_of_file; ++i) {
         if ((Poem->buffer)[i] == sym1)
             (Poem->buffer)[i] = sym2;
     }
@@ -199,9 +204,10 @@ void ReplaceSymbolInBuffer(struct Struct_Poem* Poem,
 
 ssize_t GetSizeOfFile(const char* filename)
 {
+    assert(filename != NULL);
     struct stat file_info = {};
 
-    if (stat(filename, &file_info) == -1){
+    if (stat(filename, &file_info) == -1) {
         fprintf(stderr, "Ошибка получения информации из файла |%s|", filename);
         perror("");
         return -1;
@@ -217,14 +223,14 @@ int PrintInFilePoem(struct Struct_Poem* Poem,
     assert(Poem->buffer != NULL);
     assert(Poem->poem_ptr_array != NULL);
 
-    if (out_file == NULL){
+    if (out_file == NULL) {
         fprintf(stderr, "Error opening file |%s|", out_file_name);
         perror("");
         return -1;
     }
 
     //printf("%d", Poem->number_of_lines);
-    for (size_t i = 0; i < Poem->number_of_lines; ++i){
+    for (size_t i = 0; i < Poem->number_of_lines; ++i) {
         fprintf(out_file, "%.*s",
                 Poem->poem_ptr_array[i].str_len + 1,
                 Poem->poem_ptr_array[i].line_ptr);
@@ -246,19 +252,18 @@ void FreeDataPoem(struct Struct_Poem* Poem)
 }
 
 void BubbleSort(struct Struct_Poem* Poem,
-                int (*comparator)(struct Struct_Line str1,
-                                  struct Struct_Line str2))
+                Compare_Func_t Comparator)
 {
     assert(Poem != NULL);
     assert(Poem->poem_ptr_array != NULL);
 
     for (size_t sorted_el = 0; sorted_el < Poem->number_of_lines; ++sorted_el) {
         for (size_t i = 0; i < Poem->number_of_lines - sorted_el - 1; ++i) {
-            if ((*comparator)((Poem->poem_ptr_array)[i],
-                              (Poem->poem_ptr_array)[i + 1]) > 0) {
+            if (Comparator(Poem->poem_ptr_array[i],
+                              Poem->poem_ptr_array[i + 1]) > 0) {
 
-                SwapStrings(&(Poem->poem_ptr_array[i]),
-                            &(Poem->poem_ptr_array[i + 1]));
+                SwapStrings(&Poem->poem_ptr_array[i],
+                            &Poem->poem_ptr_array[i + 1]);
             }
         }
     }
@@ -279,37 +284,33 @@ void SwapStrings(struct Struct_Line* str1,
     str2->str_len = twin_len;
 }
 
-size_t GetIndexFirstAlpha(struct Struct_Line str, const int start_point)
+size_t GoForward(struct Struct_Line str)
 {
     assert(str.line_ptr != NULL);
 
     size_t index = 0;
-    switch(start_point) {
-        case BEGINOFLINE:
-            while (!isalpha(str.line_ptr[index]))
-                index++;
-            break;
-
-        case ENDOFLINE:
-            index = str.str_len - 1;
-            while (!isalpha(str.line_ptr[index]))
-                index--;
-            break;
-
-        default:
-            break;
-    }
+    while (!isalpha(str.line_ptr[index]))
+            index++;
     return index;
 }
+
+size_t GoBackwards(struct Struct_Line str)
+{
+    assert(str.line_ptr != NULL);
+
+    size_t index = str.str_len - 1;
+    while (!isalpha(str.line_ptr[index]))
+            index--;
+}
                                                     // 1 == 2 -> 0
-int MyStrcmpByFirstChars(struct Struct_Line str1,
+int CompareByFirstChars(struct Struct_Line str1,
                          struct Struct_Line str2)  // 1 > 2 -> 1
 {                                                   // 1 < 2 -> -1
     assert(str1.line_ptr != NULL);
     assert(str2.line_ptr != NULL);
 
-    size_t index_s1 = GetIndexFirstAlpha(str1, BEGINOFLINE);
-    size_t index_s2 = GetIndexFirstAlpha(str2, BEGINOFLINE);
+    size_t index_s1 = GoForward(str1);
+    size_t index_s2 = GoForward(str2);
 
     while (str1.line_ptr[index_s1] != '\n' && str2.line_ptr[index_s2] != '\n') {
 
@@ -319,21 +320,23 @@ int MyStrcmpByFirstChars(struct Struct_Line str1,
         if (str1.line_ptr[index_s1] != '\n' && str2.line_ptr[index_s2] == '\n')
             return 1;
 
-        if (!isalpha(str1.line_ptr[index_s1])){
+        if (!isalpha(str1.line_ptr[index_s1])) {
             index_s1++;
             continue;
         }
 
-        if (!isalpha(str2.line_ptr[index_s2])){
+        if (!isalpha(str2.line_ptr[index_s2])) {
             index_s2++;
             continue;
         }
 
-        if (tolower(str1.line_ptr[index_s1]) - 'a' < tolower(str2.line_ptr[index_s2]) - 'a')
-            return -1;                    // english 'a'
+        if (tolower(str1.line_ptr[index_s1]) - 'a' <
+                    tolower(str2.line_ptr[index_s2]) - 'a')
+            return -1;                                      // english 'a'
 
-        else if (tolower(str1.line_ptr[index_s1]) - 'a' > tolower(str2.line_ptr[index_s2]) - 'a')
-            return 1;                     // english 'a'
+        else if (tolower(str1.line_ptr[index_s1]) - 'a' >
+                 tolower(str2.line_ptr[index_s2]) - 'a')
+            return 1;                                       // english 'a'
 
         index_s1++;
         index_s2++;
@@ -342,7 +345,7 @@ int MyStrcmpByFirstChars(struct Struct_Line str1,
     return 0;
 }
 
-size_t LineEndIndex(char* str){
+size_t GetLineEndIndex(char* str){
     assert(str != NULL);
 
     size_t end_index = 0;
@@ -351,14 +354,14 @@ size_t LineEndIndex(char* str){
     return --end_index;
 }
                                                  // 1 == 2 -> 0
-int MyStrcmpByLastChars(struct Struct_Line str1,
-                        struct Struct_Line str2) // 1 > 2 -> 1
+int CompareByLastChars(struct Struct_Line str1,
+                       struct Struct_Line str2) // 1 > 2 -> 1
 {                                                // 1 < 2 -> -1
     assert(str1.line_ptr != NULL);
     assert(str2.line_ptr != NULL);
 
-    size_t index_s1 = GetIndexFirstAlpha(str1, ENDOFLINE);
-    size_t index_s2 = GetIndexFirstAlpha(str2, ENDOFLINE);
+    size_t index_s1 = GoBackwards(str1);
+    size_t index_s2 = GoBackwards(str2);
 
     while (index_s1 != 0 && index_s2 != 0) {
 
@@ -368,21 +371,23 @@ int MyStrcmpByLastChars(struct Struct_Line str1,
         if (index_s1 != 0 && index_s2 == 0)
             return 1;
 
-        if (!isalpha(str1.line_ptr[index_s1])){
+        if (!isalpha(str1.line_ptr[index_s1])) {
             index_s1--;
             continue;
         }
 
-        if (!isalpha(str2.line_ptr[index_s2])){
+        if (!isalpha(str2.line_ptr[index_s2])) {
             index_s2--;
             continue;
         }
 
-        if (tolower(str1.line_ptr[index_s1]) - 'a' < tolower(str2.line_ptr[index_s2]) - 'a')
-            return -1;                    // english 'a'
+        if (tolower(str1.line_ptr[index_s1]) - 'a' <
+            tolower(str2.line_ptr[index_s2]) - 'a')
+            return -1;                                      // english 'a'
 
-        else if (tolower(str1.line_ptr[index_s1]) - 'a' > tolower(str2.line_ptr[index_s2]) - 'a')
-            return 1;                     // english 'a'
+        else if (tolower(str1.line_ptr[index_s1]) - 'a' >
+                 tolower(str2.line_ptr[index_s2]) - 'a')
+            return 1;                                       // english 'a'
 
         index_s1--;
         index_s2--;
